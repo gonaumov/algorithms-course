21aea7c70ac55b04a43efc8d540a1575e26d7b43
Georgi Naumov <gonaumov@gmail.com>
2021-08-29 09:41:51 +0300

    constructor functions and this keyword

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Algorithms course 
 
 I want just to use Jest as a helper via an algorithms course. So I am creating this tiny project. 
+
+## How to run the examples? 
+
+Well ... I doubt anyone will ever want to run these examples, 
+but if one appears can use this one liner.
---
+++ __tests__/constructor-functions-and-this-keyword.test.js
@@ -0,0 +1,12 @@
+test('constructor functions and this keyword', () => {
+    function User(firstName, lastName, age, gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+    var user1 = new User('John', 'Smith', 26, 'male');
+    console.log(user1);
+    var user200 = new User('Jill', 'Robison', 25, 'female');
+    console.log(user200);
+});
