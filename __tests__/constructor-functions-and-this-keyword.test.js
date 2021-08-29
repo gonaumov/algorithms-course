5041e4e48acdbb22ce4bb0d96a17558c12b5e692
Georgi Naumov <gonaumov@gmail.com>
2021-08-29 14:20:50 +0300

    Starting implementation of linked list

--- __tests__/constructor-functions-and-this-keyword.test.js
+++ __tests__/constructor-functions-and-this-keyword.test.js
@@ -7,7 +7,7 @@ test('constructor functions and this keyword', () => {
     }
     var user1 = new User('John', 'Smith', 26, 'male');
     console.log(user1);
-    var user200 = new User('Jill', 'Robison', 25, 'female');
+    var user200 = new User('Jill', 'Robinson', 25, 'female');
     console.log(user200);
     User.prototype.emailDomain = '@facebook.com';
     console.log(user200.emailDomain);
